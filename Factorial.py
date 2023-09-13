7958db4bbf5fb9f9aded038e8e44cb0cf5b313b9
PabloDavid2004 <pablodavidsanchezgarcia@gmail.com>
2023-09-12 23:43:48 -0600

    numero = int(input("Introduzca el número: ")) factorial = 1 i = 1 while (i <= numero):     factorial = factorial * i     i = i + 1 print ("El factorial de " + str(numero) + " es " + str(factorial))

---
+++ Factorial.py
@@ -0,0 +1,7 @@
+numero = int(input("Introduzca el número: "))
+factorial = 1
+i = 1
+while (i <= numero):
+    factorial = factorial * i
+    i = i + 1
+print ("El factorial de " + str(numero) + " es " + str(factorial))
