d4b4fe971b0bfa4b43a041f07b27a6a5e6e5eb5e
PabloDavid2004 <pablodavidsanchezgarcia@gmail.com>
2023-09-12 23:59:06 -0600

    lista=[1,5,8,7,6,3,4,11,56,86,95,74,83,110] numero = int(input("Ingrese el número a buscar en la lista: ")) if numero in lista:     print("El número está en la lista") else:     print("El número no está en la lista")

---
+++ Busqueda_en_lista.py
@@ -0,0 +1,6 @@
+lista=[1,5,8,7,6,3,4,11,56,86,95,74,83,110]
+numero = int(input("Ingrese el número a buscar en la lista: "))
+if numero in lista:
+    print("El número está en la lista")
+else:
+    print("El número no está en la lista")
